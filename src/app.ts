c84a2c61cd653bb4fbcd2dfa7e26ac39e564d9c3
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-04-23 22:24:46 +0300

    added: src

---
+++ src/app.ts
@@ -0,0 +1,61 @@
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import hpp from "hpp";
+import morgan from "morgan";
+import { CREDENTIALS, LOG_FORMAT, NODE_ENV, ORIGIN, PORT } from "./config";
+import { IRoutes } from "./interfaces";
+import { ErrorMiddleware } from "./middlewares";
+import { logger, stream } from "./utils";
+
+export class App {
+  public app: express.Application;
+  public env: string;
+  public port: string | number;
+
+  constructor(routes: IRoutes[]) {
+    this.app = express();
+    this.env = NODE_ENV || "development";
+    this.port = PORT || 3000;
+
+    this.initializeMiddlewares();
+    this.initializeRoutes(routes);
+    this.initializeErrorHandling();
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      logger.info(`=================================`);
+      logger.info(`======= ENV: ${this.env} =======`);
+      logger.info(`🚀 App listening on the port ${this.port}`);
+      logger.info(`=================================`);
+    });
+  }
+
+  public getServer() {
+    return this.app;
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(morgan(LOG_FORMAT!, { stream }));
+    this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
+    this.app.use(hpp());
+    this.app.use(helmet());
+    this.app.use(compression());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cookieParser());
+  }
+
+  private initializeRoutes(routes: IRoutes[]) {
+    routes.forEach((route) => {
+      this.app.use("/", route.router);
+    });
+  }
+
+  private initializeErrorHandling() {
+    this.app.use(ErrorMiddleware);
+  }
+}
