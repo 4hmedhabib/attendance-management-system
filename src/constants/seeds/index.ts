bb56ef80b6e425c73e2ecf4a61c7141a6ca31e4f
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-04-24 21:57:03 +0300

    added: seeds in constants

---
+++ src/constants/seeds/index.ts
@@ -0,0 +1,37 @@
+import { Prisma } from "@prisma/client";
+import slugify from "slugify";
+
+const attandanceStatuses = ["Absent", "Present", "Late", "Excused"].map(
+  (status: string) => ({
+    statusname: status,
+    statusslug: slugify(status.toLowerCase(), "_"),
+  })
+);
+
+const users = [
+  {
+    username: "ahmedhabib",
+    firstname: "Ahmed",
+    middlename: "Habib",
+    lastname: "Ahmed",
+    mobileno: "252634044833",
+    email: "info.ahmed.developer@gmail.com",
+    password: "admin1234",
+    isteacher: true,
+    isstudent: false,
+  },
+].map(
+  (user): Prisma.usersCreateManyInput => ({
+    username: user.username,
+    firstname: user.firstname,
+    middlename: user.middlename,
+    lastname: user.lastname,
+    mobileno: user.mobileno,
+    email: user.email,
+    password: user.password,
+    isteacher: user.isteacher,
+    isstudent: user.isstudent,
+  })
+);
+
+export { attandanceStatuses, users };
