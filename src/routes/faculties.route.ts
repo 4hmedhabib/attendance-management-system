40f45c0f93b88bd4fa42023e3eeb616f7f06a29b
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-04-25 22:57:16 +0300

    added: faculties in routes

---
+++ src/routes/faculties.route.ts
@@ -0,0 +1,45 @@
+import { Router } from "express";
+import { FacultyController } from "../controllers";
+import {
+  CreateFacultyDto,
+  GetFacultyBySlugDto,
+  UpdateFacultyDto,
+} from "../dtos/faculties.dto";
+import { IRoutes } from "../interfaces/";
+import { ValidationMiddleware } from "../middlewares/validation.middleware";
+
+class FacultyRoute implements IRoutes {
+  public path = "/faculties";
+  public router = Router();
+  public faculty = new FacultyController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(`${this.path}`, this.faculty.getFaculties);
+
+    this.router.get(
+      `${this.path}/detail`,
+      ValidationMiddleware(GetFacultyBySlugDto),
+      this.faculty.getFacultyBySlug
+    );
+
+    this.router.post(
+      `${this.path}`,
+      ValidationMiddleware(CreateFacultyDto),
+      this.faculty.createFaculty
+    );
+
+    this.router.put(
+      `${this.path}/:fucultySlug`,
+      ValidationMiddleware(UpdateFacultyDto),
+      this.faculty.updateFaculty
+    );
+
+    this.router.delete(`${this.path}/:fucultySlug`, this.faculty.deleteFaculty);
+  }
+}
+
+export default FacultyRoute;
