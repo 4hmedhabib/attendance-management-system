aa954a89eb7de323d94d91f483979ae1504d843a
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-04-25 22:55:34 +0300

    updated: www file

--- bin/www.ts
+++ bin/www.ts
@@ -1,9 +1,9 @@
-import { App } from '../src/app';
+import { App } from "../src/app";
+import { IndexRoutes } from "../src/routes";
 import { ValidateEnv } from "../src/utils";
 
- 
 ValidateEnv();
 
-const app = new App([]);
+const app = new App([new IndexRoutes()]);
 
 app.listen();
