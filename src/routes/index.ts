f1e00de005572620c64f8222945bb441d535a300
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-04-29 21:07:01 +0300

    updated: routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,21 @@
 import { Router } from "express";
 import { IRoutes } from "../interfaces/";
+import ClassRoute from "./classes.route";
 import FacultyRoute from "./faculties.route";
 import ShiftRoute from "./shifts.route";
+import UserRoute from "./users.route";
 
 class IndexRoutes implements IRoutes {
   public path = "/api";
   public router = Router();
 
   constructor() {
-    const routes: IRoutes[] = [new FacultyRoute(), new ShiftRoute()];
+    const routes: IRoutes[] = [
+      new UserRoute(),
+      new FacultyRoute(),
+      new ShiftRoute(),
+      new ClassRoute(),
+    ];
 
     this.initializeRoutes(routes);
   }
