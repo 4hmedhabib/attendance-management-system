67b3305e1349190cf80276793c7d1908a42085f9
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-04-29 21:05:19 +0300

    updated: controllres

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,4 @@
+export { default as ClassController } from "./classes.controller";
 export { default as FacultyController } from "./faculties.controller";
 export { default as ShiftController } from "./shifts.controller";
+export { default as UserController } from "./users.controller";
