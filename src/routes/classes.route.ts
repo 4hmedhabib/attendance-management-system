a0337d1beb93357878ae7ef88aa6bf8ce9138e12
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-04-29 21:06:48 +0300

    updated: classes routes

---
+++ src/routes/classes.route.ts
@@ -0,0 +1,55 @@
+import { Router } from "express";
+import { ClassController } from "../controllers";
+import {
+  CreateClassDto,
+  DeleteClassBySlugDto,
+  GetClassBySlugDto,
+  GetClassesDto,
+  UpdateClassDto,
+} from "../dtos";
+import { IRoutes } from "../interfaces/";
+import { ValidationMiddleware } from "../middlewares/validation.middleware";
+
+class ClassRoute implements IRoutes {
+  public path = "/classes";
+  public router = Router();
+  public class = new ClassController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(
+      `${this.path}`,
+      ValidationMiddleware(GetClassesDto),
+      this.class.getClasses
+    );
+
+    this.router.get(
+      `${this.path}/detail`,
+      ValidationMiddleware(GetClassBySlugDto),
+      this.class.getClassBySlug
+    );
+
+    this.router.post(
+      `${this.path}`,
+      ValidationMiddleware(CreateClassDto),
+      this.class.createClass
+    );
+
+    this.router.put(
+      `${this.path}/update`,
+      ValidationMiddleware(UpdateClassDto),
+      this.class.updateClass
+    );
+
+    this.router.delete(
+      `${this.path}/delete`,
+      ValidationMiddleware(DeleteClassBySlugDto),
+      this.class.deleteClass
+    );
+  }
+}
+
+export default ClassRoute;
