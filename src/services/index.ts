5e93be45d20b9c934940ced0749a3e1cb74cd504
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-04-29 21:07:41 +0300

    updated: users

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,4 @@
+export { default as ClassService } from "./classes.service";
 export { default as FacultyService } from "./faculties.service";
 export { default as ShiftService } from "./shifts.service";
+export { default as UserService } from "./users.service";
