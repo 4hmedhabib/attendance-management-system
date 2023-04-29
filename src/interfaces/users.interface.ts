dd2b066baab4c5bad7a4ccaddc4178341a999d45
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-04-29 21:06:26 +0300

    updated: users interfaces

--- src/interfaces/users.interface.ts
+++ src/interfaces/users.interface.ts
@@ -1,7 +1,42 @@
+import { IFaculty } from "./faculties.interface";
+import { IShift } from "./shifts.interface";
 export interface IUser {
   userid?: number;
-  username: string;
+  username?: string;
   firstname?: string;
   middlename?: string;
   lastname?: string;
+  mobile?: string;
+  email?: string | null;
+  password?: string;
+  ispwdupgraded?: boolean;
+  isstudent?: boolean;
+  isteacher?: boolean;
+  lastaccessdate?: Date | string | null;
+  lastchangepwd?: Date | string | null;
+  updatedusers?: IUser[];
+  createdusers?: IUser[];
+  createdshifts?: IShift;
+  createdby?: IUser;
+  updatedby?: IUser;
+  updatedshifts?: IShift;
+  facultymanager?: IFaculty;
+  facultydeputy?: IFaculty;
+}
+
+export interface IRPCreateUserPayload {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  username: string;
+  mobileNo: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  isStudent: boolean;
+  isTeacher: boolean;
+}
+
+export interface IRBCreateUser {
+  payload: IRPCreateUserPayload;
 }
