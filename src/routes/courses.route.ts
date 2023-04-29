119aa7719a24964d4cd70e2bbd4e9de830e79b1d
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-04-29 22:03:18 +0300

    added: courses in route

---
+++ src/routes/courses.route.ts
@@ -0,0 +1,55 @@
+import { Router } from "express";
+import { CourseController } from "../controllers";
+import {
+  CreateCourseDto,
+  DeleteCourseBySlugDto,
+  GetCourseBySlugDto,
+  GetCoursesDto,
+  UpdateCourseDto,
+} from "../dtos";
+import { IRoutes } from "../interfaces";
+import { ValidationMiddleware } from "../middlewares/validation.middleware";
+
+class CourseRoute implements IRoutes {
+  public path = "/courses";
+  public router = Router();
+  public course = new CourseController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(
+      `${this.path}`,
+      ValidationMiddleware(GetCoursesDto),
+      this.course.getCourses
+    );
+
+    this.router.get(
+      `${this.path}/detail`,
+      ValidationMiddleware(GetCourseBySlugDto),
+      this.course.getCourseBySlug
+    );
+
+    this.router.post(
+      `${this.path}`,
+      ValidationMiddleware(CreateCourseDto),
+      this.course.createCourse
+    );
+
+    this.router.put(
+      `${this.path}/update`,
+      ValidationMiddleware(UpdateCourseDto),
+      this.course.updateCourse
+    );
+
+    this.router.delete(
+      `${this.path}/delete`,
+      ValidationMiddleware(DeleteCourseBySlugDto),
+      this.course.deleteCourse
+    );
+  }
+}
+
+export default CourseRoute;
