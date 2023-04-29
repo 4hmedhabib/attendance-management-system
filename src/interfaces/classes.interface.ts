cc70cc04db347a42328d119c3c89f5684ea00362
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-04-29 21:06:09 +0300

    updated: classes interface

---
+++ src/interfaces/classes.interface.ts
@@ -0,0 +1,29 @@
+import { IShift } from "./shifts.interface";
+import { IUser } from "./users.interface";
+
+export interface IClass {
+  classid?: number;
+  classname?: string;
+  classslug: string;
+  description?: string | null;
+  createdat?: string | Date;
+  updatedat?: string | Date;
+  createdby?: IUser;
+  updatedby?: IUser | null;
+  shift?: IShift;
+  _count?: {
+    semisters?: number;
+    students?: number;
+  };
+}
+
+export interface IRPCreateClassPayload {
+  className: string;
+  classSlug: string;
+  description?: string;
+  shiftSlug: string;
+}
+
+export interface IRBCreateClass {
+  payload: IRPCreateClassPayload;
+}
