0ffb12cb2bbd24eaf429afb951d90d9ff5fc7754
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-05-01 19:22:25 +0300

    added: students route

---
+++ src/routes/students.route.ts
@@ -0,0 +1,55 @@
+import { Router } from "express";
+import { StudentController } from "../controllers";
+import {
+  CreateStudentDto,
+  DeleteStudentBySlugDto,
+  GetFacultiesDto,
+  GetStudentBySlugDto,
+  UpdateStudentDto,
+} from "../dtos";
+import { IRoutes } from "../interfaces";
+import { ValidationMiddleware } from "../middlewares/validation.middleware";
+
+class StudentRoute implements IRoutes {
+  public path = "/students";
+  public router = Router();
+  public student = new StudentController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.get(
+      `${this.path}`,
+      ValidationMiddleware(GetFacultiesDto),
+      this.student.getStudents
+    );
+
+    this.router.get(
+      `${this.path}/detail`,
+      ValidationMiddleware(GetStudentBySlugDto),
+      this.student.getStudentBySlug
+    );
+
+    this.router.post(
+      `${this.path}`,
+      ValidationMiddleware(CreateStudentDto),
+      this.student.createStudent
+    );
+
+    this.router.put(
+      `${this.path}/update`,
+      ValidationMiddleware(UpdateStudentDto),
+      this.student.updateStudent
+    );
+
+    this.router.delete(
+      `${this.path}/delete`,
+      ValidationMiddleware(DeleteStudentBySlugDto),
+      this.student.deleteStudent
+    );
+  }
+}
+
+export default StudentRoute;
