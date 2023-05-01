eade32e2dbd08c25a7f4b9c19e087724414ff051
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-05-01 19:23:06 +0300

    updated: teachers dto

--- src/dtos/index.ts
+++ src/dtos/index.ts
@@ -3,4 +3,6 @@ export * from "./courses.dto";
 export * from "./faculties.dto";
 export * from "./semisters.dto";
 export * from "./shifts.dto";
+export * from "./students.dto";
+export * from "./teachers.dto";
 export * from "./users.dto";
