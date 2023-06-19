19ae420afdaab9973e2d1d67fcecdbd8b7f384ce
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-06-19 20:29:09 +0300

    updated: interfaces

--- api/src/interfaces/index.ts
+++ api/src/interfaces/index.ts
@@ -3,7 +3,7 @@ export * from "./courses.interface";
 export * from "./custom.decorator";
 export * from "./faculties.interface";
 export * from "./routes.interface";
-export * from "./semisters.interface";
+export * from "./semesters.interface";
 export * from "./shifts.interface";
 export * from "./students.interface";
 export * from "./teachers.interface";
