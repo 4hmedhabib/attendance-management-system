bd80a55e1efe7c6a4ad534ffce0c81565169a909
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-06-19 20:27:49 +0300

    updated: controller

--- api/src/controllers/index.ts
+++ api/src/controllers/index.ts
@@ -1,7 +1,7 @@
 export { default as ClassController } from "./classes.controller";
 export { default as CourseController } from "./courses.controller";
 export { default as FacultyController } from "./faculties.controller";
-export { default as SemisterController } from "./semisters.controller";
+export { default as SemesterController } from "./semesters.controller";
 export { default as ShiftController } from "./shifts.controller";
 export { default as StudentController } from "./students.controller";
 export { default as TeacherController } from "./teachers.controller";
