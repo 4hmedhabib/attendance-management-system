41dc7c8a89e8003ff527e19cc78176607a3a178b
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-06-19 20:29:35 +0300

    updated: routes

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -3,7 +3,7 @@ import { IRoutes } from "../interfaces/";
 import ClassRoute from "./classes.route";
 import CourseRoute from "./courses.route";
 import FacultyRoute from "./faculties.route";
-import SemisterRoute from "./semisters.route";
+import SemesterRoute from "./semesters.route";
 import ShiftRoute from "./shifts.route";
 import StudentRoute from "./students.route";
 import TeacherRoute from "./teachers.route";
@@ -19,7 +19,7 @@ class IndexRoutes implements IRoutes {
       new FacultyRoute(),
       new ShiftRoute(),
       new ClassRoute(),
-      new SemisterRoute(),
+      new SemesterRoute(),
       new CourseRoute(),
       new StudentRoute(),
       new TeacherRoute(),
