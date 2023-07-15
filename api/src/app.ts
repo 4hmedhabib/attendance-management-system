4c2381307c71575580706f9819ff118b6e22d696
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-07-15 18:13:40 +0300

    updated: app

--- api/src/app.ts
+++ api/src/app.ts
@@ -19,7 +19,7 @@ export class App {
   constructor(routes: IRoutes[]) {
     this.app = express();
     this.env = NODE_ENV || "development";
-    this.port = PORT || 3000;
+    this.port = PORT || 3001;
 
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
