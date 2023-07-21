5d6746bb42af20b1cddeade8b7129867ad88f43c
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-07-21 11:27:09 +0300

    updated: prisma migrations

---
+++ api/prisma/migrations/20230721035951_updated_users_is_admin/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "isadmin" BOOLEAN NOT NULL DEFAULT false;
---
+++ api/prisma/migrations/20230721052550_updated_users_and_facutlies_with_1_m/migration.sql
@@ -0,0 +1,5 @@
+-- DropIndex
+DROP INDEX "faculties_deputyid_key";
+
+-- DropIndex
+DROP INDEX "faculties_managerid_key";
