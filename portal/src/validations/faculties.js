84f52ddfeb234b6d305e6bf10883918139782996
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-07-21 19:31:58 +0300

    updated: faculties validations

--- portal/src/validations/faculties.js
+++ portal/src/validations/faculties.js
@@ -16,3 +16,20 @@ export const createFacultySchema = yup.object({
     })
     .optional(),
 });
+
+export const updateFacultySchema = yup.object({
+  facultyName: yup.string().required(),
+  description: yup.string(),
+  manager: yup
+    .object({
+      label: yup.string().required(),
+      value: yup.string().required(),
+    })
+    .required(),
+  deputy: yup
+    .object({
+      label: yup.string().optional(),
+      value: yup.string().optional(),
+    })
+    .optional(),
+});
