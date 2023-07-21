ef9557de777de1a2cba81a5f29a74c4377d09430
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-07-21 11:29:20 +0300

    added: res error component

---
+++ portal/src/components/Common/ResError.jsx
@@ -0,0 +1,35 @@
+import { isArray } from "lodash";
+
+const ResError = ({ error }) => {
+  return error ? (
+    typeof error === "object" ? (
+      isArray(error) ? (
+        error.map((err, idx) => {
+          return (
+            <div className="bg-danger-subtle text-danger text-center py-1 my-1 rounded">
+              <span key={idx}>
+                {`${Object.keys(err)[0]}: ${Object.values(err)}`}
+              </span>
+            </div>
+          );
+        })
+      ) : (
+        <div className="bg-danger-subtle text-danger text-center py-1 my-1 rounded">
+          {Object.keys(error).map((key, idx) => {
+            return (
+              <span key={idx}>
+                `${key}: ${error[key]}`
+              </span>
+            );
+          })}
+        </div>
+      )
+    ) : (
+      <div className="bg-danger-subtle text-danger text-center py-1 my-1 rounded">
+        <span>{error}</span>
+      </div>
+    )
+  ) : null;
+};
+
+export default ResError;
