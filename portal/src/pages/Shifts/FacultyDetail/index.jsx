fde0f9a7966f878e4687b5c43d0cc69f96f8bbae
4hmedhabib <info.ahmed.developer@gmail.com>
2023-07-22 21:30:49 +0300

    updated: shifts

---
+++ portal/src/pages/Shifts/FacultyDetail/index.jsx
@@ -0,0 +1,319 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { Card, CardBody, Col, Container, Row, Spinner } from "reactstrap";
+
+//import images
+import { useFormik } from "formik";
+import { Link, useLocation } from "react-router-dom";
+import { toast } from "react-toastify";
+import slugify from "slugify";
+import urls from "../../../api/urls";
+import NotFound from "../../../components/Common/NotFound";
+import ResError from "../../../components/Common/ResError";
+import useApiCall from "../../../hooks/apiHook";
+import { updateFacultySchema } from "../../../validations/faculties";
+import Sidebar from "./Sidebar";
+import UpdateForm from "./UpdateForm";
+
+const FacultyDetail = () => {
+  document.title = "Faculty Detail | FFU ATMS";
+
+  const { state } = useLocation();
+
+  if (!state?.facultySlug) {
+    return (
+      <NotFound>
+        <h3>Sorry!, Faculty Id Not Found</h3>
+        <Link className="btn btn-outline-info mt-3" to={"/faculties"}>
+          Go All Faculties
+        </Link>
+      </NotFound>
+    );
+  }
+
+  const [faculty, setFaculty] = useState(null);
+  const [isEdit, setIsEdit] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setErrors] = useState(null);
+
+  const {
+    data: facultyData,
+    isError: facultyIsErr,
+    isLoading: facultyIsLoading,
+    errMsg: facultyErrMsg,
+    refetch: facultyRefetch,
+  } = useApiCall("FACULTY_DETAIL", urls.facultyDetail(), {
+    payload: {
+      isMiniView: false,
+      facultySlug: state.facultySlug,
+    },
+  });
+
+  const {
+    data: classesData,
+    isError: classesIsErr,
+    isLoading: classesIsLoading,
+    errMsg: classesErrMsg,
+    refetch: classesRefetch,
+  } = useApiCall(
+    "CLASSES_FACULTY_DETAIL",
+    urls.classes(),
+    {
+      payload: {
+        isMiniView: true,
+        filters: {
+          facultySlug: state.facultySlug,
+        },
+      },
+    },
+    false
+  );
+
+  const {
+    data: usersData,
+    isError: usersIsErr,
+    isLoading: usersIsLoading,
+    errMsg: usersErrMsg,
+    refetch: usersRefetch,
+  } = useApiCall(
+    "USERS_FACULTY_DETAIL",
+    urls.users(),
+    {
+      payload: {
+        isMiniView: true,
+        filters: {
+          isAdmin: true,
+        },
+      },
+    },
+    false
+  );
+
+  const { update: updateFaculty } = useApiCall(
+    "UPDATE_FACULTY",
+    urls.updateFaculty(),
+    {
+      payload: {
+        isMiniView: true,
+        filters: {
+          facultySlug: state.facultySlug,
+        },
+      },
+    },
+    false
+  );
+
+  useEffect(() => {
+    if (facultyData) {
+      setFaculty(facultyData.data);
+    }
+  }, [facultyData]);
+
+  const onRefresh = useCallback(() => {
+    facultyRefetch({
+      payload: {
+        isMiniView: false,
+        facultySlug: state.facultySlug,
+      },
+    });
+    usersRefetch({
+      payload: {
+        isMiniView: true,
+        filters: {
+          isAdmin: true,
+        },
+      },
+    });
+    classesRefetch({
+      payload: {
+        isMiniView: true,
+        filters: {
+          facultySlug: state.facultySlug,
+        },
+      },
+    });
+  }, []);
+
+  useEffect(() => {
+    let subscribed = true;
+    if (subscribed) {
+      onRefresh();
+    }
+
+    () => {
+      subscribed = false;
+    };
+  }, [onRefresh]);
+
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      facultyName: faculty?.facultyname ?? "",
+      description: faculty?.description ?? "",
+      manager: faculty?.manager
+        ? {
+            label: `${faculty?.manager?.firstname} ${faculty?.manager?.middlename} ${faculty?.manager?.lastname}`,
+            value: faculty?.manager.username,
+          }
+        : undefined,
+      deputy: undefined,
+    },
+    validationSchema: updateFacultySchema,
+    onSubmit: async (values) => {
+      setErrors(null);
+      setIsSubmitting(true);
+      toast.loading("Please wait a few minutes...", {
+        toastId: "updateFaculty",
+      });
+
+      const payload = {
+        facultySlug: state?.facultySlug,
+        data: {
+          facultyName: values.facultyName,
+          facultySlug: slugify(values.facultyName?.toLowerCase(), "_"),
+          description: values.description,
+          manager: values.manager.value,
+          deputy: values?.deputy?.value ?? "",
+        },
+      };
+
+      await updateFaculty({ payload })
+        .then((res) => {
+          setErrors(null);
+
+          toast.update("updateFaculty", {
+            isLoading: false,
+            type: "success",
+            render: "Successfully Faculty Updated: " + values.facultyName,
+            autoClose: 3000,
+            closeOnClick: true,
+          });
+
+          setIsSubmitting(false);
+          onRefresh();
+        })
+        .catch((err) => {
+          console.log("----++++-");
+          toast.update("updateFaculty", {
+            isLoading: false,
+            type: "error",
+            autoClose: 5000,
+            render: "Something went wrong!",
+            closeOnClick: true,
+          });
+
+          setErrors(err.response.data?.message || err.message);
+
+          setIsSubmitting(false);
+        });
+    },
+  });
+
+  const onEdit = () => {
+    setIsEdit(!isEdit);
+  };
+
+  if (facultyIsLoading) {
+    return (
+      <div className="page-content ">
+        <Col lg={12} className="text-center mt-5">
+          <Spinner />
+        </Col>
+      </div>
+    );
+  } else if (facultyIsErr) {
+    return <ResError error={facultyErrMsg} />;
+  }
+
+  if (!faculty) {
+    return (
+      <NotFound>
+        <h3>Sorry!, Faculty Not Found</h3>
+        <Link className="btn btn-outline-info mt-3" to={"/faculties"}>
+          Go All Faculties
+        </Link>
+      </NotFound>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <div className="page-content">
+        <Container fluid>
+          <Row>
+            <Col lg={12}>
+              {error && <ResError error={error} />}
+              <Card className="mx-n4 mt-n4 bg-info bg-white">
+                <CardBody>
+                  <div className="text-center mb-4">
+                    <h5 className="mt-3 mb-1">{faculty?.facultyname}</h5>
+                    <p className="text-muted mb-3">Faculty</p>
+                  </div>
+
+                  <div className="d-flex align-items-center">
+                    <ul className="list-unstyled hstack gap-3 mb-0 flex-grow-1"></ul>
+                    <div className="hstack gap-2">
+                      <button
+                        className="btn btn-info d-flex justify-content-center align-items-center"
+                        onClick={onRefresh}
+                      >
+                        <i className="mdi mdi-reload align-baseline me-1"></i>
+                        Refresh
+                      </button>
+                      <button
+                        type="button"
+                        className={`btn ${
+                          isEdit ? "btn-danger" : "btn-warning"
+                        } d-flex justify-content-center align-items-center`}
+                        onClick={onEdit}
+                      >
+                        {!isEdit ? (
+                          <>
+                            <i className="bx bxs-edit align-baseline me-1"></i>
+                            Edit
+                          </>
+                        ) : (
+                          <>
+                            <i className="bx bx bx-x align-baseline me-1"></i>
+                            Cancel
+                          </>
+                        )}
+                      </button>
+                      <Link
+                        className="btn btn-secondary d-flex justify-content-center align-items-center"
+                        to={"/faculties"}
+                      >
+                        <i className="bx bx-left-arrow align-baseline me-1"></i>
+                        Back
+                      </Link>
+                    </div>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+
+          <Row>
+            <Sidebar faculty={faculty} />
+            <UpdateForm
+              faculty={faculty}
+              facultySlug={state?.facultySlug}
+              isEdit={isEdit}
+              classesData={classesData}
+              classesIsErr={classesIsErr}
+              classesErrMsg={classesErrMsg}
+              classesIsLoading={classesIsLoading}
+              usersData={usersData}
+              usersIsErr={usersIsErr}
+              usersIsLoading={usersIsLoading}
+              usersErrMsg={usersErrMsg}
+              formik={formik}
+              isSubmitting={isSubmitting}
+            />
+          </Row>
+        </Container>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default FacultyDetail;
--- portal/src/pages/Shifts/index.jsx
+++ portal/src/pages/Shifts/index.jsx
@@ -1,27 +1,159 @@
-import React from "react";
-import { Container } from "reactstrap";
-//Import Breadcrumb
+import "bootstrap/dist/css/bootstrap.min.css";
+import dayjs from "dayjs";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import TableContainer from "../../components/Common/TableContainer";
+
+//import components
 import Breadcrumbs from "../../components/Common/Breadcrumb";
 
-//i18n
-import { withTranslation } from "react-i18next";
-const Shift = (props) => {
+import { useNavigate } from "react-router-dom";
+import { Button, Card, CardBody, Col, Row } from "reactstrap";
+import urls from "../../api/urls";
+import ResError from "../../components/Common/ResError";
+import useApiCall from "../../hooks/apiHook";
+
+function Faculty() {
   //meta title
-  document.title = "Shift | FFU - ATMS & Shift Template";
+  document.title = "Faculties | FFU - ATMS";
+
+  const [faculties, setFaculties] = useState([]);
+
+  const {
+    data,
+    isError,
+    isLoading,
+    errMsg,
+    refetch: facultiesRefetch,
+  } = useApiCall(
+    "FACULTY_LIST",
+    urls.faculties(),
+    {
+      payload: { isMiniView: false },
+    },
+    false
+  );
+
+  const navigate = useNavigate();
+
+  const handleFacultyClicks = () => {
+    navigate("create");
+  };
+
+  useEffect(() => {
+    if (data) {
+      setFaculties(data.data);
+    }
+  }, [data]);
+
+  const onRefresh = useCallback(() => {
+    facultiesRefetch({
+      payload: { isMiniView: false },
+    });
+  }, []);
+
+  useEffect(() => {
+    onRefresh();
+  }, [onRefresh]);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "ID",
+        accessor: "facultyid",
+        style: {
+          textAlign: "center",
+          width: "10%",
+          background: "#0000",
+        },
+        Cell: ({ cell }) => {
+          return cell.value;
+        },
+      },
+      {
+        Header: "Faculty Name",
+        accessor: "facultyname",
+        Cell: ({ cell }) => {
+          return cell?.value;
+        },
+      },
+      {
+        Header: "Created By",
+        accessor: "createdby",
+        Cell: ({ cell }) => {
+          return `${cell?.value.firstname} ${cell?.value.middlename}`;
+        },
+      },
+      {
+        Header: "Classes",
+        accessor: "_count.classes",
+        Cell: ({ cell }) => {
+          return cell?.value;
+        },
+      },
+      {
+        Header: "Date",
+        accessor: "createdat",
+        Cell: ({ cell }) => {
+          return dayjs(cell?.value).format("YYYY-MM-DD");
+        },
+      },
+      {
+        Header: "View",
+        accessor: "facultyslug",
+        disableFilters: true,
+        Cell: ({ cell }) => {
+          return (
+            <Button
+              type="button"
+              color="primary"
+              className="btn-sm btn-rounded"
+              onClick={() =>
+                navigate("detail", { state: { facultySlug: cell?.value } })
+              }
+            >
+              View
+            </Button>
+          );
+        },
+      },
+    ],
+    []
+  );
 
   return (
     <React.Fragment>
       <div className="page-content">
-        <Container fluid>
-          {/* Render Breadcrumb */}
-          <Breadcrumbs
-            title={props.t("Shifts")}
-            breadcrumbItem={props.t("Shift")}
-          />
-        </Container>
+        <div className="container-fluid">
+          <Breadcrumbs title="Faculties" breadcrumbItem="Faculties List" />
+
+          {isError && <ResError error={errMsg} />}
+
+          <Row>
+            <Col xs="12">
+              <Card>
+                <CardBody>
+                  <TableContainer
+                    columns={columns}
+                    data={faculties || []}
+                    isGlobalFilter={false}
+                    isAddOptions={true}
+                    handleClick={handleFacultyClicks}
+                    customPageSize={50}
+                    className="custom-header-css"
+                    isLoading={isLoading && !isError}
+                    onRefresh={onRefresh}
+                    title="Add New Faculty"
+                  />
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
       </div>
     </React.Fragment>
   );
-};
+}
 
-export default withTranslation()(Shift);
+export default Faculty;
+export { default as CreateFaculty } from "./CreateFaculty";
+export { default as FacultyDetail } from "./FacultyDetail";
