a30a657c90e3f98dece3df0c27e25f37cf57b89f
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-07-25 19:32:50 +0300

    added: student list and create student

--- portal/src/pages/Students/index.jsx
+++ portal/src/pages/Students/index.jsx
@@ -1,27 +1,185 @@
-import React from "react";
-import { Container } from "reactstrap";
-//Import Breadcrumb
+import "bootstrap/dist/css/bootstrap.min.css";
+import dayjs from "dayjs";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+
+//import components
 import Breadcrumbs from "../../components/Common/Breadcrumb";
 
-//i18n
-import { withTranslation } from "react-i18next";
-const Dashboard = (props) => {
+import { useNavigate } from "react-router-dom";
+import { Button, Card, CardBody, Col, Row } from "reactstrap";
+import urls from "../../api/urls";
+import ResError from "../../components/Common/ResError";
+import TableContainer from "../../components/Common/TableContainer";
+import useApiCall from "../../hooks/apiHook";
+
+function Student() {
   //meta title
-  document.title = "Dashboard | FFU - ATMS & Dashboard Template";
+  document.title = "Students | FFU - ATMS";
+
+  const [students, setStudents] = useState([]);
+
+  const {
+    data,
+    isError,
+    isLoading,
+    errMsg,
+    refetch: studentsRefetch,
+  } = useApiCall(
+    "STUDENT_LIST",
+    urls.students(),
+    {
+      payload: { isMiniView: false },
+    },
+    false
+  );
+
+  const navigate = useNavigate();
+
+  const handleStudentClicks = () => {
+    navigate("create");
+  };
+
+  useEffect(() => {
+    if (data) {
+      setStudents(data.data);
+    }
+  }, [data]);
+
+  const onRefresh = useCallback(() => {
+    studentsRefetch({
+      payload: { isMiniView: false },
+    });
+  }, []);
+
+  useEffect(() => {
+    onRefresh();
+  }, [onRefresh]);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "ID",
+        accessor: "studentid",
+        style: {
+          textAlign: "center",
+          width: "10%",
+          background: "#0000",
+        },
+        Cell: ({ cell }) => {
+          return cell.value;
+        },
+      },
+      {
+        Header: "Student ID",
+        accessor: "stdid",
+        Cell: ({ cell }) => {
+          console.log(cell);
+          return `${cell?.value?.toUpperCase()}`;
+        },
+      },
+      {
+        Header: "First Name",
+        accessor: "firstname",
+        Cell: ({ cell }) => {
+          console.log(cell);
+          return `${cell?.value}`;
+        },
+      },
+      {
+        Header: "Middle Name",
+        accessor: "middlename",
+        Cell: ({ cell }) => {
+          console.log(cell);
+          return `${cell?.value}`;
+        },
+      },
+      {
+        Header: "Last Name",
+        accessor: "lastname",
+        Cell: ({ cell }) => {
+          console.log(cell);
+          return `${cell?.value}`;
+        },
+      },
+      {
+        Header: "Mobile No",
+        accessor: "mobileno",
+        Cell: ({ cell }) => {
+          return cell?.value;
+        },
+      },
+      {
+        Header: "Created By",
+        accessor: "createdby",
+        Cell: ({ cell }) => {
+          return `${cell?.value.firstname} ${cell?.value.middlename}`;
+        },
+      },
+      {
+        Header: "Date",
+        accessor: "createdat",
+        Cell: ({ cell }) => {
+          return dayjs(cell?.value).format("YYYY-MM-DD");
+        },
+      },
+      {
+        Header: "View",
+        accessor: "stdid",
+        id: "Viewstdid",
+        disableFilters: true,
+        Cell: ({ cell }) => {
+          return (
+            <Button
+              type="button"
+              color="primary"
+              className="btn-sm btn-rounded"
+              onClick={() =>
+                navigate("detail", { state: { studentId: cell?.value } })
+              }
+            >
+              View
+            </Button>
+          );
+        },
+      },
+    ],
+    []
+  );
 
   return (
     <React.Fragment>
       <div className="page-content">
-        <Container fluid>
-          {/* Render Breadcrumb */}
-          <Breadcrumbs
-            title={props.t("Dashboards")}
-            breadcrumbItem={props.t("Dashboard")}
-          />
-        </Container>
+        <div className="container-fluid">
+          <Breadcrumbs title="Students" breadcrumbItem="Students List" />
+
+          {isError && <ResError error={errMsg} />}
+
+          <Row>
+            <Col xs="12">
+              <Card>
+                <CardBody>
+                  <TableContainer
+                    columns={columns}
+                    data={students || []}
+                    isGlobalFilter={false}
+                    isAddOptions={true}
+                    handleClick={handleStudentClicks}
+                    customPageSize={50}
+                    className="custom-header-css"
+                    isLoading={isLoading && !isError}
+                    onRefresh={onRefresh}
+                    title="Add New Student"
+                  />
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
       </div>
     </React.Fragment>
   );
-};
+}
 
-export default withTranslation()(Dashboard);
+export default Student;
+export { default as CreateStudent } from "./CreateStudent";
+export { default as StudentDetail } from "./StudentDetail";
