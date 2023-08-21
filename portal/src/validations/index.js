e0ab781400e652ac5193a48f55adb4d9e045cce4
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-08-21 12:04:09 +0300

    updated: validations

--- portal/src/validations/index.js
+++ portal/src/validations/index.js
@@ -2,6 +2,7 @@ export * from "./classes";
 export * from "./courses";
 export * from "./faculties";
 export * from "./semesters";
+export * from "./sessions";
 export * from "./shifts";
 export * from "./students";
 export * from "./teachers";
