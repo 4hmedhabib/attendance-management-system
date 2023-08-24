5d44dab3dacf6a3d70cdafc4c188eee640a35bb2
4hmedhabib <info.ahmed.developer@gmail.com>
2023-08-24 22:49:54 +0300

    updated: src

--- api/src/app.ts
+++ api/src/app.ts
@@ -10,11 +10,14 @@ import { LOG_FORMAT, NODE_ENV, PORT } from "./config";
 import { IRoutes } from "./interfaces";
 import { ErrorMiddleware } from "./middlewares";
 import { logger, stream } from "./utils";
+import session from "express-session";
+import * as config from "./config";
 
 export class App {
   public app: express.Application;
   public env: string;
   public port: string | number;
+  private cookieConfig = {};
 
   constructor(routes: IRoutes[]) {
     this.app = express();
@@ -52,7 +55,27 @@ export class App {
     this.app.use(compression());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
-    this.app.use(cookieParser());
+
+    this.app.use(cookieParser("abc", this.cookieConfig));
+
+    this.app.use(
+      session({
+        name: "FFU_ATMS",
+        secret: "abc6241a56sd1fa6sdf46a5",
+        genid: (req) => {
+          return this.sessionUniqueID();
+        },
+        saveUninitialized: false,
+        resave: false,
+        rolling: true,
+        cookie: {
+          sameSite: false,
+          secure: config.isSecure || false,
+          maxAge: 1000,
+          httpOnly: true,
+        },
+      })
+    );
   }
 
   private initializeRoutes(routes: IRoutes[]) {
@@ -63,5 +86,17 @@ export class App {
 
   private initializeErrorHandling() {
     this.app.use(ErrorMiddleware);
+  }
+
+  private sessionUniqueID() {
+    let length = 10;
+    let tmp = 'm$%eaoCuaZPMRQygKjz+."mh0}.([c';
+    let value = "";
+
+    for (let i = 0; i < length; i++) {
+      value += tmp.charAt(Math.floor(Math.random() * tmp.length));
+    }
+
+    return value;
   }
 }
