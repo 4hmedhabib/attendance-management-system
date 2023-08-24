e10c55ed7a16f4032f9baf85676da49f5b473e1e
4hmedhabib <info.ahmed.developer@gmail.com>
2023-08-24 22:48:14 +0300

    updated: config

--- api/src/config/index.ts
+++ api/src/config/index.ts
@@ -1,6 +1,11 @@
 import { config } from "dotenv";
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const CREDENTIALS = process.env.CREDENTIALS === "true";
+const CREDENTIALS = process.env.CREDENTIALS === "true";
 export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } =
   process.env;
+
+const isSecure = false;
+const maxSessionExpire = 30 * 60000; // 30 min
+
+export { isSecure, CREDENTIALS, maxSessionExpire };
