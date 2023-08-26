8fd0a342ec50ddc968c7ac837c35b3d91f0f76b3
ahmedhabiib <info.ahmed.developer@gmail.com>
2023-08-26 06:31:42 +0300

    added: permission middleware

--- portal/src/routes/route.jsx
+++ portal/src/routes/route.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import NotFound from "../pages/Utility/NotFound";
 
 const AuthMiddleware = (props) => {
-  if (!localStorage.getItem("authUser")) {
+  const authUser = JSON.parse(localStorage.getItem("authUser"));
+
+  if (!authUser || !authUser?.user || !authUser?.group) {
     return (
       <Navigate to={{ pathname: "/login", state: { from: props.location } }} />
     );
   }
 
   return <React.Fragment>{props.children}</React.Fragment>;
+};
+
+export const AuthPermMiddleware = ({
+  allowedPerms = [],
+  children,
+  isRoute = false,
+}) => {
+  const authUser = JSON.parse(localStorage.getItem("authUser"));
+
+  if (!authUser) return null;
+  const isAllowed = allowedPerms.includes(authUser?.group?.groupslug);
+
+  return !isAllowed ? isRoute ? <NotFound /> : null : children;
 };
 
 export default AuthMiddleware;
